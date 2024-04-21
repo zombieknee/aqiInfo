8deff0650ae69989d2e0d7ec78d63d31738003de
Clinton Sexton <sextonclinton@gmail.com>
2024-04-21 10:58:53 -0400

    Initial Commit

---
+++ AirQualityInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirQualityInfo
+//
+//  Created by Clinton Sexton on 4/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
