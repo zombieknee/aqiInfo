1924892efe8333a62f40958e8ffcf91b07407a81
Clinton Sexton <sextonclinton@gmail.com>
2024-04-25 14:19:22 -0400

    changed color and info displaying strat, removed button and city feature for future ref

--- AirQualityInfo/AQI.swift
+++ AirQualityInfo/AQI.swift
@@ -9,15 +9,20 @@ import UIKit
 import CoreLocation
 
 
-class AQI: UIViewController, CLLocationManagerDelegate, UITableViewDelegate, UITableViewDataSource {
-
+class AQI: UIViewController, CLLocationManagerDelegate {
+    
+    enum LoadError: Error {
+        case fetchFailed, decodeFailed
+    }
+    
+    
     @IBOutlet weak var latitude: UILabel!
     @IBOutlet weak var longitude: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var todayView: UITextView!
+    @IBOutlet weak var tomorrowView: UITextView!
+    @IBOutlet weak var yesterdayView: UITextView!
     @IBOutlet weak var sationIDLabel: UILabel!
-    @IBOutlet weak var infoTable: UITableView!
-    @IBOutlet weak var button: UIButton!
-    @IBOutlet weak var gpsButton: UIButton!
     
     let locationManager = CLLocationManager()
     var currentLocValue = CLLocationCoordinate2D()
@@ -54,22 +59,20 @@ class AQI: UIViewController, CLLocationManagerDelegate, UITableViewDelegate, UIT
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.locationManager.requestWhenInUseAuthorization()
         guard let locValue: CLLocationCoordinate2D = locationManager.location?.coordinate else { return }
-       
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        
         setupTime()
         
         Task {
-            let formatter = DateFormatter()
-            formatter.dateFormat = "yyyy-MM-dd"
             
             let aqiToday = try await restReq.airNowAPIToday(withLat: "\(locValue.latitude)", andLong: "\(locValue.longitude)")
-            
             let aqiForcast = try await restReq.airNowAPIForcast(withLat: "\(locValue.latitude)",
                                                                 andLong: "\(locValue.longitude)",
                                                                 forDay: formatter.date(from: tomorrow) ?? today)
-            
             let aqiYesterday  = try await restReq.airNowAPIForcast(withLat: "\(locValue.latitude)",
                                                                    andLong: "\(locValue.longitude)",
                                                                    forDay: formatter.date(from: yesterday) ?? today)
@@ -83,41 +86,23 @@ class AQI: UIViewController, CLLocationManagerDelegate, UITableViewDelegate, UIT
             
             
             self.todayInfo = "The AQI is \(aqiToday[0].AQI) for \(aqiToday[0].ParameterName) which is \(aqiToday[0].Category.Name) for Today"
-            self.yesterdayInfo = "The AQI is \(aqiYesterday[0].AQI) for \(aqiYesterday[0].ParameterName) which is \(aqiYesterday[0].Category.Name) for Yesterday"
+            self.yesterdayInfo = "The AQI was \(aqiYesterday[0].AQI) for \(aqiYesterday[0].ParameterName) which was \(aqiYesterday[0].Category.Name) for Yesterday"
             self.tomorrowInfo = "The AQI is \(aqiForcast[0].AQI) for \(aqiForcast[0].ParameterName) which is \(aqiForcast[0].Category.Name) for Tomorrow"
+            
+            todayView.text = todayInfo
+            tomorrowView.text = tomorrowInfo
+            yesterdayView.text = yesterdayInfo
             
             cityLabel.text = aqiToday[0].ReportingArea
             let stationCode = stationCodes.features
             
-            sationIDLabel.text = stationCode[0].properties.stationIdentifier //"TEMP ID" // TODO: need to get this from https://github.com/weather-gov/api/discussions/478
+            sationIDLabel.text = stationCode[0].properties.stationIdentifier 
         }
         
         // Do any additional setup after loading the view.
         latitude.text = String(format: "%.3f", locValue.latitude)
         longitude.text = String(format: "%.3f", locValue.longitude)
     }
-    
-       
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
-    }
-    
-    //MARK: - TableView Delgate Methods -
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell: UITableViewCell?
-
-        if indexPath.row == tableView.numberOfRows(inSection: 0) - 1 {
-            cell = tableView.dequeueReusableCell(withIdentifier: "AddSetCell")
-        } 
-        else {
-            cell = tableView.dequeueReusableCell(withIdentifier: "SetCell")
-        }
-
-        return cell!
-    }
+ 
 }
 
