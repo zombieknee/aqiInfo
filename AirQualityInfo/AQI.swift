15688309ba69ac0ae8c5987e78ee76552e067dbd
Clinton Sexton <sextonclinton@gmail.com>
2024-04-25 12:06:27 -0400

    basic error handling, getting first AQI measurement for each day

--- AirQualityInfo/AQI.swift
+++ AirQualityInfo/AQI.swift
@@ -9,15 +9,13 @@ import UIKit
 import CoreLocation
 
 
-class AQI: UIViewController, CLLocationManagerDelegate {
+class AQI: UIViewController, CLLocationManagerDelegate, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var latitude: UILabel!
     @IBOutlet weak var longitude: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var sationIDLabel: UILabel!
-    @IBOutlet weak var yesterdayView: UIView!
-    @IBOutlet weak var todayView: UIView!
-    @IBOutlet weak var tomorrowView: UIView!
+    @IBOutlet weak var infoTable: UITableView!
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var gpsButton: UIButton!
     
@@ -25,78 +23,101 @@ class AQI: UIViewController, CLLocationManagerDelegate {
     var currentLocValue = CLLocationCoordinate2D()
     let restReq = RestController()
     
-    @IBAction func testAPI(_ sender: Any) {
-        print("Button Press")
-        Task { @MainActor in
-            guard let locValue: CLLocationCoordinate2D = locationManager.location?.coordinate else { return }
-            try await restReq.getLatestMeasurementByCoord(withCoord: "\(locValue.latitude),\(locValue.longitude)")
+    var todayInfo = String()
+    var tomorrowInfo = String()
+    var yesterdayInfo = String()
+    
+    var today = Date()
+    var tomorrow = String()
+    var yesterday = String()
+    
+    func setupTime() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        if let tmp = Calendar.current.date(byAdding: .day, value: 1, to: today) {
+            tomorrow = formatter.string(from:tmp)
+        }
+        else {
+            let alert = UIAlertController(title: "Alert", message: "Time has broken", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        if let tmp = Calendar.current.date(byAdding: .day, value: -1, to: today) {
+            yesterday = formatter.string(from:tmp)
+        }
+        else {
+            let alert = UIAlertController(title: "Alert", message: "Time has broken", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
         }
     }
-    
-    @IBAction func getCurrentLocationButtonPressed() {
-       getLocation()
-     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.locationManager.requestWhenInUseAuthorization()
         guard let locValue: CLLocationCoordinate2D = locationManager.location?.coordinate else { return }
+       
+        setupTime()
+        
         Task {
-            try await restReq.getLatestMeasurementByCoord(withCoord: "\(locValue.latitude),\(locValue.longitude)")
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd"
+            
+            let aqiToday = try await restReq.airNowAPIToday(withLat: "\(locValue.latitude)", andLong: "\(locValue.longitude)")
+            
+            let aqiForcast = try await restReq.airNowAPIForcast(withLat: "\(locValue.latitude)",
+                                                                andLong: "\(locValue.longitude)",
+                                                                forDay: formatter.date(from: tomorrow) ?? today)
+            
+            let aqiYesterday  = try await restReq.airNowAPIForcast(withLat: "\(locValue.latitude)",
+                                                                   andLong: "\(locValue.longitude)",
+                                                                   forDay: formatter.date(from: yesterday) ?? today)
+            
+            let station = try await restReq.getWeatherStationLoc(forLat: "\(locValue.latitude)", andLong: "\(locValue.longitude)")
+            
+            let stationCodes = try await restReq.getWeatherStationCode(forGrid: station.properties.gridId,
+                                                                       xPos: "\(station.properties.gridX)",
+                                                                       yPos: "\(station.properties.gridY)")
+            
+            
+            
+            self.todayInfo = "The AQI is \(aqiToday[0].AQI) for \(aqiToday[0].ParameterName) which is \(aqiToday[0].Category.Name) for Today"
+            self.yesterdayInfo = "The AQI is \(aqiYesterday[0].AQI) for \(aqiYesterday[0].ParameterName) which is \(aqiYesterday[0].Category.Name) for Yesterday"
+            self.tomorrowInfo = "The AQI is \(aqiForcast[0].AQI) for \(aqiForcast[0].ParameterName) which is \(aqiForcast[0].Category.Name) for Tomorrow"
+            
+            cityLabel.text = aqiToday[0].ReportingArea
+            let stationCode = stationCodes.features
+            
+            sationIDLabel.text = stationCode[0].properties.stationIdentifier //"TEMP ID" // TODO: need to get this from https://github.com/weather-gov/api/discussions/478
         }
         
-        
-        currentLocValue = locValue
         // Do any additional setup after loading the view.
-        latitude.text = String(format: "%.3f", currentLocValue.latitude)
-        longitude.text = String(format: "%.3f", currentLocValue.longitude)
-        
-//        cityLabel =
+        latitude.text = String(format: "%.3f", locValue.latitude)
+        longitude.text = String(format: "%.3f", locValue.longitude)
     }
     
-     func getLocation() {
-         guard CLLocationManager.locationServicesEnabled() else {
-             print("Location services are disabled on your device. In order to use this app, go to " +
-                   "Settings → Privacy → Location Services and turn location services on.")
-             return
-         }
-         
-         guard let locValue: CLLocationCoordinate2D = locationManager.location?.coordinate else { return }
-         print("locations = \(locValue.latitude) \(locValue.longitude)")
-         let authStatus = locationManager.authorizationStatus
-         guard authStatus == .authorizedWhenInUse else {
-               switch authStatus {
-               case .denied, .restricted:
-                   print("This app is not authorized to use your location. In order to use this app, " +
-                     "go to Settings → AirQualityInfo → Location and select the \"While Using " +
-                     "the App\" setting.")
-                   
-                 case .notDetermined:
-                   locationManager.requestWhenInUseAuthorization()
-                   
-                 default:
-                   print("Oops! Shouldn't have come this far.")
-               }
-               
-               return
-             }
-         locationManager.delegate = self
-         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-         locationManager.startUpdatingLocation()
-       }
        
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
-//        print("locations = \(locValue.latitude) \(locValue.longitude)")
     }
     
-    // MARK: - CLLocationManagerDelegate methods
+    //MARK: - TableView Delgate Methods -
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
     
-    // This is called if:
-    // - the location manager is updating, and
-    // - it was able to get the user's location.\
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell?
 
-   
+        if indexPath.row == tableView.numberOfRows(inSection: 0) - 1 {
+            cell = tableView.dequeueReusableCell(withIdentifier: "AddSetCell")
+        } 
+        else {
+            cell = tableView.dequeueReusableCell(withIdentifier: "SetCell")
+        }
+
+        return cell!
+    }
 }
 
--- AirQualityInfo/RestController.swift
+++ AirQualityInfo/RestController.swift
@@ -9,119 +9,144 @@ import Foundation
 import UIKit
 
 
-struct JSONResult: Decodable {
-//    var meta: Dictionary
-    let results: [AQIResult]
-
+// MARK: - Weather.gov Api handling
+struct  WeatherGovJSON: Codable {
+    let properties: StationLocation
 }
 
-struct AQIResult: Decodable {
-    let location: String
-    let city: String?
-    let country: String
-    let coordinates: Coords
-    let measurements: [AQIMeasurement]
-}
-struct AQIMeasurement:Decodable {
-    let parameter: String
-    let value: Double
-    let lastUpdated: String
-    let unit: String
-    let sourceName: String
+struct StationLocation: Codable {
+    let gridId: String
+    let gridX: Int
+    let gridY: Int
 }
 
-struct Coords: Codable {
-    let latitude: Double
-    let longitude: Double
+struct WeatherGovStationJSON: Codable {
+    let features: [StationInfo]
 }
 
+struct StationInfo: Codable {
+    let properties: StationProperties
+}
 
+struct StationProperties: Codable {
+    let stationIdentifier: String
+}
 
+// MARK: - AirNow Api Handling
+struct AirNowForecastResult: Codable {
+    let DateForecast: String
+    let ParameterName: String
+    let AQI: Int
+    let Category: Category
+}
+
+struct AirNowResult: Codable {
+    let DateObserved: String
+    let ReportingArea: String
+    let Latitude: Double
+    let Longitude: Double
+    let ParameterName: String
+    let AQI: Int
+    let Category: Category
+}
+struct Category: Codable {
+    let Name: String
+}
+
+enum GenericError: Error {
+        case unknown
+}
 class RestController{
-    let appDelegate = UIApplication.shared.delegate as! AppDelegate
-
-    func getCitties() async throws {
-        let url = URL(string: "https://api.openaq.org/v2/cities")!
+    
+    func airNowAPIForcast(withLat lat: String, andLong long: String, forDay day: Date) async throws -> [AirNowForecastResult] {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        let strDay = formatter.string(from: day)
+        guard let url = URL(string: "https://www.airnowapi.org/aq/forecast/latLong/") else { throw URLError(.unsupportedURL) }
         var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
         let queryItems: [URLQueryItem] = [
-            URLQueryItem(name: "limit", value: "5"),
-            URLQueryItem(name: "page", value: "1"),
-            URLQueryItem(name: "offset", value: "0"),
-            URLQueryItem(name: "sort", value: "asc"),
-            URLQueryItem(name: "order_by", value: "city"),
+            URLQueryItem(name: "format", value: "application/json"),
+            URLQueryItem(name: "latitude", value: lat),
+            URLQueryItem(name: "longitude", value: long),
+            URLQueryItem(name: "date", value: strDay),
+            URLQueryItem(name: "api_key", value: "4407AAEE-4201-4F2F-B7EE-953C0776B812")
         ]
+        
         components.queryItems = components.queryItems.map { $0 + queryItems } ?? queryItems
         
         var request = URLRequest(url: components.url!)
         request.httpMethod = "GET"
         request.timeoutInterval = 10
         request.allHTTPHeaderFields = ["accept": "application/json"]
-       
+        
         let (data, response) = try await URLSession.shared.data(for: request)
-        print(String(decoding: data, as: UTF8.self))
-
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw GenericError.unknown
+        }
+        
+        return try JSONDecoder().decode([AirNowForecastResult].self, from: data)
     }
-    
-    func getLocationByID () async throws{
-
-        let url = URL(string: "https://api.openaq.org/v2/locations/2167")!
+    func airNowAPIToday(withLat lat: String, andLong long: String) async throws -> [AirNowResult]{
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        let strDay = formatter.string(from: Date())
+        guard let url = URL(string: "https://www.airnowapi.org/aq/observation/latLong/current") else { throw URLError(.unsupportedURL)}
         var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
         let queryItems: [URLQueryItem] = [
-          URLQueryItem(name: "limit", value: "100"),
-          URLQueryItem(name: "page", value: "1"),
-          URLQueryItem(name: "offset", value: "0"),
-          URLQueryItem(name: "sort", value: "asc"),
+            URLQueryItem(name: "format", value: "application/json"),
+            URLQueryItem(name: "latitude", value: lat),
+            URLQueryItem(name: "longitude", value: long),
+            URLQueryItem(name: "date", value: strDay),
+            URLQueryItem(name: "api_key", value: "4407AAEE-4201-4F2F-B7EE-953C0776B812")
         ]
+        
         components.queryItems = components.queryItems.map { $0 + queryItems } ?? queryItems
-
+        
         var request = URLRequest(url: components.url!)
         request.httpMethod = "GET"
         request.timeoutInterval = 10
         request.allHTTPHeaderFields = ["accept": "application/json"]
-
-        let (data, response) = try await URLSession.shared.data(for: request)
-        print(String(decoding: data, as: UTF8.self))
-    }
-    
-    func getLatestMeasurementByCoord(withCoord coords: String) async throws {
-        let url = URL(string: "https://api.openaq.org/v1/latest")!
-        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
-        let queryItems: [URLQueryItem] = [
-            URLQueryItem(name: "limit", value: "100"),
-            URLQueryItem(name: "page", value: "1"),
-            URLQueryItem(name: "offset", value: "0"),
-            URLQueryItem(name: "sort", value: "desc"),
-            URLQueryItem(name: "coordinates", value: "\(coords)"),
-            URLQueryItem(name: "radius", value: "5000"),
-            URLQueryItem(name: "order_by", value: "lastUpdated"),
-            URLQueryItem(name: "dump_raw", value: "false"),
-        ]
-        components.queryItems = components.queryItems.map { $0 + queryItems } ?? queryItems
-
-        var request = URLRequest(url: components.url!)
-        request.httpMethod = "GET"
-        request.timeoutInterval = 10
-        request.allHTTPHeaderFields = [
-          "accept": "application/json",
-          "content-type": "application/json"
-        ]
-
-        let (data, response) = try await URLSession.shared.data(for: request)
-        let jres: JSONResult = try! JSONDecoder().decode(JSONResult.self, from: data)
         
-        print (jres)
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw GenericError.unknown
+        }
+        
+        return try JSONDecoder().decode([AirNowResult].self, from: data)
+        
     }
-    
-    func getLatestMeasuermentByID(withLocID locID: String) async throws {
-        let url = URL(string: "https://api.openaq.org/v1/latest/\(locID)")!
+    func getWeatherStationLoc(forLat lat: String, andLong long: String) async throws -> WeatherGovJSON {
+        guard let url = URL(string: "https://api.weather.gov/points/\(lat),\(long)") else {  throw URLError(.unsupportedURL) }
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         request.timeoutInterval = 10
         request.allHTTPHeaderFields = ["accept": "application/json"]
         
         let (data, response) = try await URLSession.shared.data(for: request)
-        print(String(decoding: data, as: UTF8.self))
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw GenericError.unknown
+        }
+        
+        return try JSONDecoder().decode(WeatherGovJSON.self, from: data)
     }
-}
+    
+    func getWeatherStationCode(forGrid grid: String, xPos gridX: String, yPos gridY: String) async throws -> WeatherGovStationJSON {
+        guard let url = URL(string: "https://api.weather.gov/gridpoints/\(grid)/\(gridX),\(gridY)/stations") else { throw URLError(.unsupportedURL) }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.timeoutInterval = 10
+        request.allHTTPHeaderFields = ["accept": "application/json"]
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw GenericError.unknown
+        }
+        
+        return try JSONDecoder().decode(WeatherGovStationJSON.self, from: data)
+    }
 
+}
 
